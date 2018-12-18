6653340020c871a44bda2a3ba287d7a0a889639e
Apakoh_Quentin-Gerome <quentin.bartschs@gmail.com>
2018-12-18 11:08:30 +0100

    Test

--- src/main/java/SimpleJDBC/DataSourceFactory.java
+++ src/main/java/SimpleJDBC/DataSourceFactory.java
@@ -9,15 +9,10 @@ import javax.sql.DataSource;
 public class DataSourceFactory {
     
     	public static DataSource getDataSource() {
-		org.apache.derby.jdbc.ClientDataSource ds = new org.apache.derby.jdbc.ClientDataSource();
-		ds.setDatabaseName("sample");
-		ds.setUser("app");
-		ds.setPassword("app");
-		// The host on which Network Server is running
-		ds.setServerName("localhost");
-		// port on which Network Server is listening
-		ds.setPortNumber(1527);
-		return ds;
+		org.apache.derby.jdbc.EmbeddedDataSource es = new org.apache.derby.jdbc.EmbeddedDataSource();
+                es.setCreateDatabase("create");
+                es.setDatabaseName("embedded_sample");
+                return es;
 	}	
     
 }
